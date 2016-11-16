6b48b55fc2daa349b101563d7813081a78bc5a38
tristan <yeonsunghoon@gmail.com>
2016-11-17 02:27:06 +0900

    ongoing SettingsActivity

--- app/src/main/java/com/nnm/team91/mine/SettingsActivity.java
+++ app/src/main/java/com/nnm/team91/mine/SettingsActivity.java
@@ -1,29 +1,46 @@
 package com.nnm.team91.mine;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 import java.util.List;
 
-public class SettingsActivity extends PreferenceActivity {
-
+public class SettingsActivity extends AppCompatActivity {
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (hasHeaders()) {
-            Button button = new Button(this);
-            button.setText("Some action");
-            setListFooter(button);
+        getFragmentManager().beginTransaction().add(android.R.id.content,
+                new PrefsFragment()).commit();
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("뒤로가기");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기버튼 생성
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF5D5D5D)); // 액션바 색상
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
         }
     }
 
-    @Override
-    public void onBuildHeaders(List<Header> target) {// onCreate보다 먼저 호출되는 콜백 메소드, 액티비티를 생성시점에 한번 호출
-        loadHeadersFromResource(R.xml.preference_headers, target);
-    }
-
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }// 뒤로가기 이벤트
 }
 
 
