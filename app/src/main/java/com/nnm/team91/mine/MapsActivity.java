68e10d438dfec2652e936f822c3bbd0775e2e3bc
tristan <yeonsunghoon@gmail.com>
2016-11-23 00:38:03 +0900

    Completed MultiWindow

--- app/src/main/java/com/nnm/team91/mine/MapsActivity.java
+++ app/src/main/java/com/nnm/team91/mine/MapsActivity.java
@@ -20,8 +20,8 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-  /*      MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+/*
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);*/
     }
 /*
