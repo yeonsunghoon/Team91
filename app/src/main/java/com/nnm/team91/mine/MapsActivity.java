ceb2645160b55fc001101a86c87f68bdab291c1f
tristan <yeonsunghoon@gmail.com>
2016-11-22 22:31:14 +0900

    ongoing MapsActivity: instant Run
    reference URL: http://webnautes.tistory.com/1056

--- app/src/main/java/com/nnm/team91/mine/MapsActivity.java
+++ app/src/main/java/com/nnm/team91/mine/MapsActivity.java
@@ -3,11 +3,34 @@ package com.nnm.team91.mine;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapsActivity extends FragmentActivity {
+
+    static final LatLng SEOUL = new LatLng(37.56, 126.97);
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+  /*      MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);*/
     }
+/*
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        googleMap = map;
+
+        Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }*/
 }
